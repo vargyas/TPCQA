bed8c096c87d241d14e1ff5805a8a3c3b2a062c1
Marton Vargyas <m.vargyas@gmail.com>
2017-04-27 16:30:42 +0200

    fixed indent again

--- mgainframe.h
+++ mgainframe.h
@@ -10,38 +10,38 @@ class MGainFrame
 RQ_OBJECT("MGainFrame")
 
 private:
-	Int_t               fIPlot; ///< Select what to draw on main canvas by clicking on tab (0: map of gain, 1: profile plots)
-	TGTransientFrame    * fMain;
-	TGPictureButton     * load, * save, * help;
-	TGHorizontalFrame   * toolbar;
-	TGStatusBar         * fStatusBar;
-	TGTab               * fTab;
-	TGCompositeFrame    * fCF[3];
-	Double_t fXmin, fYmin;//, fXRight, fYLeft, fYRight;//
+    Int_t               fIPlot; ///< Select what to draw on main canvas by clicking on tab (0: map of gain, 1: profile plots)
+    TGTransientFrame    * fMain;
+    TGPictureButton     * load, * save, * help;
+    TGHorizontalFrame   * toolbar;
+    TGStatusBar         * fStatusBar;
+    TGTab               * fTab;
+    TGCompositeFrame    * fCF[3];
+    Double_t fXmin, fYmin;//, fXRight, fYLeft, fYRight;//
 
 protected:
-	MGain               * fGain; // 1: unsegmented (bottom panel), 0: segmented side (top panel)
-	TCanvas             * fCanv;
-	TRootEmbeddedCanvas * fEcanvasAll[3];
-	TPad                * fPad[5][3]; ///< Four pads make space for labels, on three tabs
+    MGain               * fGain; // 1: unsegmented (bottom panel), 0: segmented side (top panel)
+    TCanvas             * fCanv;
+    TRootEmbeddedCanvas * fEcanvasAll[3];
+    TPad                * fPad[5][3]; ///< Four pads make space for labels, on three tabs
 
 public:
-	MGainFrame(const TGWindow *p, const TGWindow *main, UInt_t w, UInt_t h);
-	virtual ~MGainFrame();
+    MGainFrame(const TGWindow *p, const TGWindow *main, UInt_t w, UInt_t h);
+    virtual ~MGainFrame();
 
-	void LoadFile();
-	void DoTab(Int_t);
+    void LoadFile();
+    void DoTab(Int_t);
 
-	void AdjustPad(TPad * pad);
-	void AdjustMapPad(TPad * pad);
-	void CreateDividedPad();
+    void AdjustPad(TPad * pad);
+    void AdjustMapPad(TPad * pad);
+    void CreateDividedPad();
 
-	void DrawMaps();
-	void DrawProfiles();
+    void DrawMaps();
+    void DrawProfiles();
 
-	void EventInfo(Int_t event, Int_t px, Int_t py, TObject * selected);
-	void Save();
-	void Help();
+    void EventInfo(Int_t event, Int_t px, Int_t py, TObject * selected);
+    void Save();
+    void Help();
 };
 
 #endif // MGAINFRAME_H
