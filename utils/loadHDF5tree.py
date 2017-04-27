fe2c775a43d331be03c1ef866de9a753bf2e2600
Marton Vargyas <m.vargyas@gmail.com>
2017-04-27 16:19:11 +0200

    loading ellipse axes, diameter->d

--- utils/loadHDF5tree.py
+++ utils/loadHDF5tree.py
@@ -36,37 +36,16 @@ def convert_dict_nparray(dictionary, key):
             tmp_array = list()
             for line in dictionary[key]:
                 line = tuple(line)
-                # only keep x, y, area, diameter, moment_1, moment_2
-                #sliceObj = slice(0, 1, 6, 7) 
-                tmp_array.append( (line[0], line[1], line[6], line[7], line[12], line[13]) )
+                tmp_array.append( (line[0], line[1], line[7], line[19], line[18]) )
             
             # create a named list which will be directly
             # converted to a TTree object
             my_array = np.array( tmp_array, 
                     dtype=[('x',np.float64),
                         ('y',np.float64),
-#                        ('bb_x',np.float64),
-#                        ('bb_y',np.float64),
-#                        ('bb_width',np.float64),
-#                        ('bb_height',np.float64),
-                        ('area',np.float64),
-                        ('diameter',np.float64),
-#                        ('perimColour_B',np.float64),
-#                        ('perimColour_G',np.float64),
-#                        ('perimColour_R',np.float64),
-#                        ('ellipse_angle',np.float64),
-                        ('moment_1',np.float64),
-                        ('moment_2',np.float64),
-#                        ('moment_3',np.float64),
-#                        ('moment_4',np.float64),
-#                        ('moment_5',np.float64),
-#                        ('moment_6',np.float64),
-#                        ('ellipse_big_axis',np.float64),
-#                        ('ellipse_small_axis',np.float64),
-#                        ('bb_Colour_B',np.float64),
-#                        ('bb_Colour_G',np.float64),
-#                        ('bb_Colour_R',np.float64),
-#                        ('bool_0=background_1=foreground',np.bool)
+                        ('d',np.float64),
+                        ('a', np.float64),
+                        ('b',np.float64)
                         ] )
             return my_array
         except:
@@ -96,30 +75,30 @@ M A I N   P R O G R A M
 """
 
 # Creating empty container for data (the ugly way)
-data_array_inner = np.array( [(0,0,0,0,0,0)],
+data_array_inner = np.array( [(0,0,0,0,0)],
                     dtype=[('x',np.float64),
                         ('y',np.float64),
-                        ('area',np.float64),
-                        ('diameter',np.float64), 
-                        ('moment_1',np.float64), 
-                        ('moment_2',np.float64)] )
-data_array_outer = np.array( [(0,0,0,0,0,0)],
+                        ('d',np.float64),
+                        ('a',np.float64),
+                        ('b',np.float64) ] )
+data_array_outer = np.array( [(0,0,0,0,0)],
                     dtype=[('x',np.float64),
                         ('y',np.float64),
-                        ('area',np.float64),
-                        ('diameter',np.float64),
-                        ('moment_1',np.float64), 
-                        ('moment_2',np.float64)] )
+                        ('d',np.float64),
+                        ('a',np.float64),
+                        ('b',np.float64) ] )
 
 
 # Loop over the chunks
 print '========================='
 ifile = 0
 datadir = sys.argv[1]
+outname = "outfile.root"
 if len(sys.argv) > 2:
     outname = sys.argv[2]
 
 for ifilename in glob.iglob('{}/*.h5'.format(datadir)):
+    ifile += 1
     print 'load file:', ifile,'/',len(glob.glob('{}/*.h5'.format(datadir))), ':', ifilename
     # load HDF5 into a python dictionary
     data_dict = load_HDF5file_dict(ifilename)
@@ -127,36 +106,18 @@ for ifilename in glob.iglob('{}/*.h5'.format(datadir)):
     data_array_tmp_inner = convert_dict_nparray(data_dict, 'inner')
     data_array_tmp_outer = convert_dict_nparray(data_dict, 'outer')
 
-
     # merge that with the empty container defined outside of the loop
     data_array_inner = np.concatenate( (data_array_tmp_inner, data_array_inner), axis=0 )
     data_array_outer = np.concatenate( (data_array_tmp_outer, data_array_outer), axis=0 )
 
-    ifile += 1
+
     print '========================='
 
-outname = "outfile.root"
+
 # create a tree from the array
 print '\ncreate a tree from the array'
 treeInner = array2tree(data_array_inner, name='inner_tree')
 treeOuter = array2tree(data_array_outer, name='outer_tree')
-
-
-print '\ncalculate rim width'
-rim_tmp = list()
-# for i in range(treeInner.GetEntries()):
-for i in range(len(data_array_outer)):
-    rim=data_array_outer[i]['diameter']-data_array_inner[i]['diameter']
-    rim_tmp.append((data_array_outer[i]['x'], data_array_outer[i]['y'], rim))
-
-data_array_rim = np.array( rim_tmp,
-                    dtype=[('x',np.float64),
-                        ('y',np.float64),
-                        ('rim',np.float64)] )
-treeRim = array2tree(data_array_rim, name='rim_tree')
-
-# merge the trees (takes some time) so it is not used
-#tree = TTree.MergeTrees( tlist )
 
 print 'writing file to:', outname
 outFile = TFile(os.path.join(datadir,outname), 'RECREATE')
@@ -164,8 +125,8 @@ outFile.cd()
 
 treeInner.Write()
 treeOuter.Write()
-treeRim.Write()
 
 outFile.Write()
 outFile.Close()
+
 
