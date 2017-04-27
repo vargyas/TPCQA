6a5af002b0e9f797bbab1ca7f07dfea79822dcf1
Marton Vargyas <m.vargyas@gmail.com>
2017-04-27 16:23:26 +0200

    fixed indent

--- mgain.h
+++ mgain.h
@@ -17,22 +17,22 @@ class MGain
 {
 RQ_OBJECT("MGain")
   private:
-	TString fInFileName[2];
-	TFile * fInFile[2];
-	TTree * fTree[2]; ///< Tree loaded from 0: measurement, 1: reference
-	TH2D * fhMap[3]; ///< 0: Gain map of measurement, 1: reference, 2: actual gain
-	TH1D * fProfMean[2];
+    TString fInFileName[2];
+    TFile * fInFile[2];
+    TTree * fTree[2]; ///< Tree loaded from 0: measurement, 1: reference
+    TH2D * fhMap[3]; ///< 0: Gain map of measurement, 1: reference, 2: actual gain
+    TH1D * fProfMean[2];
 
   public:
-	MGain();
-	virtual ~MGain();
-	void LoadFile(TString infilename, Int_t i);
-	void Save();
-	void DrawMap(TPad * p, Int_t which);
-	void DrawProfile(TPad * p, Int_t which);
+    MGain();
+    virtual ~MGain();
+    void LoadFile(TString infilename, Int_t i);
+    void Save();
+    void DrawMap(TPad * p, Int_t which);
+    void DrawProfile(TPad * p, Int_t which);
 
-	void Clear(); ///< Since first load loads the mesurement, second the reference,
-				  ///< it is convenient to reset the counter if one wants to reload each.
+    void Clear(); ///< Since first load loads the mesurement, second the reference,
+                  ///< it is convenient to reset the counter if one wants to reload each.
 };
 
 #endif // MGAIN_H
