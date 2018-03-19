d08347c679f2e733762a20ac48c286d3a3d1d9b3
vargyas <m.vargyas@gmail.com>
2018-03-19 16:28:49 +0100

    Fixing data loss
    TFile should be created after TTree, otherwise it would be filled with garbage, resulting in possible data loss

--- utils/loadHDF5tree.py
+++ utils/loadHDF5tree.py
@@ -4,8 +4,9 @@
 ### Files are stored in HDF5 format, details are in ALICE*.recipe.
 ### It reads them into a ROOT tree and saves them into a ROOT file.
 ###
-### python loadHDF5tree.py [directory path which contain the hdf5 files]
-###  it will create outfile.root in same directory with the ROOT trees
+### python loadHDF5tree.py [hdf5 dir] [optional: ROOT file name with full path]
+###   if ROOT file name is not specified, it will 
+###   create outfile.root in the home directory
 ###
 ### Author: Vargyas, Marton
 ### Email: mvargyas@cern.ch
@@ -13,7 +14,7 @@
 #########################################################################
 
 from ROOT import TFile, TTree
-import tables, os, glob
+import tables, glob
 import sys
 from array import array
 
@@ -25,10 +26,7 @@ datadir = sys.argv[1]
 outname = "~/outfile.root"
 if len(sys.argv) > 2:
     outname = sys.argv[2]
-outFile = TFile(os.path.join(datadir,outname), 'RECREATE')
-outFile = TFile(outname, 'RECREATE')
-outFile.cd()
-
+    
 # Generate trees
 treenames = ["blocked", "defect", "etching", "inner", "outer"]
 tree = []
@@ -47,13 +45,14 @@ for it in range(len(treenames)):
     tree[it].Branch('a', _a, 'a/D')
     tree[it].Branch('b', _b, 'b/D')
     tree[it].Branch('fl', _fl, 'fl/D')          
-      
+
 for ifilename in glob.iglob('{}/*.h5'.format(datadir)):
     ifile += 1
-    print 'load file:', ifile,'/',len(glob.glob('{}/*.h5'.format(datadir))), ':', ifilename
+    print('load file:', ifile,'/',len(glob.glob('{}/*.h5'.format(datadir))), ':', ifilename)
     h5file = tables.open_file(ifilename, mode='r')
 
     data={} 
+    
     for idx, group in enumerate(h5file.walk_groups()):
         if idx>0:
             flavour=group._v_name
@@ -69,8 +68,12 @@ for ifilename in glob.iglob('{}/*.h5'.format(datadir)):
                 _a[0]=a[i]; _b[0]=b[i]; _fl[0]=fl[i]
                 tree[idx-1].Fill()
     h5file.close()
-
+    
 print( '=========================')
+
+#outFile = TFile(os.path.join(datadir,outname), 'RECREATE')
+outFile = TFile(outname, 'RECREATE')
+outFile.cd()
 for it in tree:
     it.Write()
 
