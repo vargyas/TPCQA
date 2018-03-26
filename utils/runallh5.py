e3c8373b109256cd79f15d6c67614954cf3028cc
vargyas <m.vargyas@gmail.com>
2018-03-26 11:05:01 +0200

    combined hdf5 conversion with report generation

--- utils/runallh5.py
+++ utils/runallh5.py
@@ -1,9 +1,9 @@
 import os
-import time
+#import time
 
-def GuessFoilName(name):
+def GuessFoilName(name, method):
     fName = name
-    fName=fName.replace("/results_conv4","")
+    fName=fName.replace("/{}".format(method),"")
     fName = os.path.basename(fName)
     
     # remove budapest conventions
@@ -14,29 +14,35 @@ def GuessFoilName(name):
 
     return fName
 
+method = "results_svm" 
+#method = "results_conv4"
 
-dirs = next(os.walk('/media/gridserv/opticalScan/'))[1]
+scandir='/mnt/alicetpc/storage/opticalScan/'
+datadir='/mnt/alicetpc/storage/opticalData/'
+
+dirs = next(os.walk(scandir))[1]
 dir_all = []
 dir_proc = []
-fOutDir = "/home/vargyas/cernbox/Work/ALICE/serviceWork/OS";
+#fOutDir = "/home/vargyas/cernbox/Work/ALICE/serviceWork/OS";
+fOutDir = datadir+method;
 
 # looping over all directories
 # determine which was processed with optical recognition
 for idir in dirs:
     # select in which are we interested in
     if 'I-' in idir or 'O2-' in idir:
-        subdirs = os.listdir('/media/gridserv/opticalScan/'+idir)
+        subdirs = os.listdir(scandir+idir)
         for isubdir in subdirs:
-            if isubdir=='results_conv4':
+            if isubdir == method: 
                 dir_all.append(idir)
 
 # looping over processed directories and run hdf5 converter
 # on those which were not converted already
 for idir in dir_all:
 
-    fDataDir = "/media/gridserv/opticalScan/"+idir+"/results_conv4";
+    fDataDir = scandir+idir+"/"+method;
     # ROOTname has to be determined
-    fName = GuessFoilName(fDataDir);
+    fName = GuessFoilName(fDataDir,method);
     ROOTName = "{}/{}.root".format(fOutDir, fName);
 
     # if ROOT file exists, skip processing
@@ -45,20 +51,34 @@ for idir in dir_all:
 
     dir_proc.append(idir)
 
-print len(dir_proc), "folders will be converted"
+print(dir_proc)
+print("{} folders will be converted".format(len(dir_proc)))
 
 count = 1
+ROOTNames = []
 for idir in dir_proc:
-    print 'PROCESSING FOLDER ', count, '/', len(dir_proc), ':'
+    print('PROCESSING FOLDER ', count, '/', len(dir_proc), ':')
 
-    fDataDir = "/media/gridserv/opticalScan/"+idir+"/results_conv4";
+    #fDataDir = "/media/gridserv/opticalScan/"+idir+"/results_conv4";
+    fDataDir = scandir+idir+"/"+method;
     # ROOTname has to be determined
-    fName = GuessFoilName(fDataDir);
+    fName = GuessFoilName(fDataDir,method);
     ROOTName = "{}/{}.root".format(fOutDir, fName);
-
+    ROOTNames.append("{}.root".format(fName))
     # This program creates a ROOT tree from the optical scan's hdf5 file output
     execute = "python loadHDF5tree.py {} {}".format(fDataDir,ROOTName);
-    print "Processing command:\n", execute 
+    print( "Processing command:\n", execute )
     os.system(execute)
     count += 1
+
+
+# After conversion finished, run the optical processing on the ROOT files
+# Note that only for the segmented side, the optreport finds the unsegmented 
+#    assuming the same name and substitutes s->u.
+for iroot in ROOTNames:
+    if "-s" in iroot:
+        execute = "../optreport {}".format(iroot)
+        print("Processing command:\n", execute)
+        os.system(execute)
+
 
